77762c18fe08332b73c96c73996312cb375979d3
hichri-louay <louay.hichri@hotmail.com>
2024-09-09 21:29:26 +0100

    add the hook and context to save the extension selected by user : reef or reef-wallet-connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 
+import React, {useState} from 'react';
 import {
-  Components
+  Components,
+  hooks
 } from '@reef-chain/react-lib';
 import {
   extension
-} from '@reef-chain/util-lib'
+} from '@reef-chain/util-lib';
+
 
 import './App.css';
+import useConnectedWallet from './hooks/useConnectedWallet';
 
 export const availableWallOptions = [
   Components.walletSelectorOptions[extension.REEF_EXTENSION_IDENT],
   Components.walletSelectorOptions[extension.REEF_WALLET_CONNECT_IDENT],
 ]
+
+
 function App() {
+  const {selExtensionName,setSelExtensionName} = useConnectedWallet();
   
+  const onExtensionSelected = async(ident: string) => {
+    console.log('extension changed to ', ident);
+    setSelExtensionName(ident);
+  }
+
   return (
     <div className="App">
+        {
+          selExtensionName && <div>Selected Extension: {selExtensionName}</div>
+        }
         <Components.WalletSelector 
           availableExtensions={availableWallOptions}
-          onExtensionSelect={(ext: string) => console.log({ext})}
+          onExtensionSelect={(ext: string) => setSelExtensionName(ext)}
         />
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ConnectedWalletProvider } from './context/ConnectedWalletContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ConnectedWalletProvider>
+      <App />
+    </ConnectedWalletProvider>
+    
   </React.StrictMode>
 );
 
