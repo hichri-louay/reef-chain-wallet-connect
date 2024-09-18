4198d079d896be66c38bf2ab412552e046168dac
hichri-louay <louay.hichri@hotmail.com>
2024-09-19 00:54:02 +0100

    remove logs

--- src/App.tsx
+++ src/App.tsx
@@ -19,195 +19,7 @@ export const convertToReadableFormat = (value) => {
   const decimalValue = BigInt(!!value ? value : 0);
   return decimalValue / BigInt(Math.pow(10, 18));
 };
-export const getDAOToken = (account, network) => {
-  if (network === "testnet") {
-    const contractAddress = "0x9001369C17044CA19b4376182aaD81BBCB01Ba1c";
-    console.log({ account: account });
-    const contractAbi = [
-      {
-        type: "constructor",
-        inputs: [
-          { name: "initialSupply", type: "uint256", internalType: "uint256" },
-        ],
-        stateMutability: "nonpayable",
-      },
-      {
-        name: "ERC20InsufficientAllowance",
-        type: "error",
-        inputs: [
-          { name: "spender", type: "address", internalType: "address" },
-          { name: "allowance", type: "uint256", internalType: "uint256" },
-          { name: "needed", type: "uint256", internalType: "uint256" },
-        ],
-      },
-      {
-        name: "ERC20InsufficientBalance",
-        type: "error",
-        inputs: [
-          { name: "sender", type: "address", internalType: "address" },
-          { name: "balance", type: "uint256", internalType: "uint256" },
-          { name: "needed", type: "uint256", internalType: "uint256" },
-        ],
-      },
-      {
-        name: "ERC20InvalidApprover",
-        type: "error",
-        inputs: [
-          { name: "approver", type: "address", internalType: "address" },
-        ],
-      },
-      {
-        name: "ERC20InvalidReceiver",
-        type: "error",
-        inputs: [
-          { name: "receiver", type: "address", internalType: "address" },
-        ],
-      },
-      {
-        name: "ERC20InvalidSender",
-        type: "error",
-        inputs: [{ name: "sender", type: "address", internalType: "address" }],
-      },
-      {
-        name: "ERC20InvalidSpender",
-        type: "error",
-        inputs: [{ name: "spender", type: "address", internalType: "address" }],
-      },
-      {
-        name: "Approval",
-        type: "event",
-        inputs: [
-          {
-            name: "owner",
-            type: "address",
-            indexed: true,
-            internalType: "address",
-          },
-          {
-            name: "spender",
-            type: "address",
-            indexed: true,
-            internalType: "address",
-          },
-          {
-            name: "value",
-            type: "uint256",
-            indexed: false,
-            internalType: "uint256",
-          },
-        ],
-        anonymous: false,
-      },
-      {
-        name: "Transfer",
-        type: "event",
-        inputs: [
-          {
-            name: "from",
-            type: "address",
-            indexed: true,
-            internalType: "address",
-          },
-          {
-            name: "to",
-            type: "address",
-            indexed: true,
-            internalType: "address",
-          },
-          {
-            name: "value",
-            type: "uint256",
-            indexed: false,
-            internalType: "uint256",
-          },
-        ],
-        anonymous: false,
-      },
-      {
-        name: "allowance",
-        type: "function",
-        inputs: [
-          { name: "owner", type: "address", internalType: "address" },
-          { name: "spender", type: "address", internalType: "address" },
-        ],
-        outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
-        stateMutability: "view",
-      },
-      {
-        name: "approve",
-        type: "function",
-        inputs: [
-          { name: "spender", type: "address", internalType: "address" },
-          { name: "value", type: "uint256", internalType: "uint256" },
-        ],
-        outputs: [{ name: "", type: "bool", internalType: "bool" }],
-        stateMutability: "nonpayable",
-      },
-      {
-        name: "balanceOf",
-        type: "function",
-        inputs: [{ name: "account", type: "address", internalType: "address" }],
-        outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
-        stateMutability: "view",
-      },
-      {
-        name: "decimals",
-        type: "function",
-        inputs: [],
-        outputs: [{ name: "", type: "uint8", internalType: "uint8" }],
-        stateMutability: "view",
-      },
-      {
-        name: "name",
-        type: "function",
-        inputs: [],
-        outputs: [{ name: "", type: "string", internalType: "string" }],
-        stateMutability: "view",
-      },
-      {
-        name: "symbol",
-        type: "function",
-        inputs: [],
-        outputs: [{ name: "", type: "string", internalType: "string" }],
-        stateMutability: "view",
-      },
-      {
-        name: "totalSupply",
-        type: "function",
-        inputs: [],
-        outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
-        stateMutability: "view",
-      },
-      {
-        name: "transfer",
-        type: "function",
-        inputs: [
-          { name: "to", type: "address", internalType: "address" },
-          { name: "value", type: "uint256", internalType: "uint256" },
-        ],
-        outputs: [{ name: "", type: "bool", internalType: "bool" }],
-        stateMutability: "nonpayable",
-      },
-      {
-        name: "transferFrom",
-        type: "function",
-        inputs: [
-          { name: "from", type: "address", internalType: "address" },
-          { name: "to", type: "address", internalType: "address" },
-          { name: "value", type: "uint256", internalType: "uint256" },
-        ],
-        outputs: [{ name: "", type: "bool", internalType: "bool" }],
-        stateMutability: "nonpayable",
-      },
-    ];
 
-    const contract = new Contract(contractAddress, contractAbi, account.signer);
-
-    // Create contract instance using the Reef signer
-    return contract;
-  }
-  return undefined;
-};
 
 export const { WalletSelector, walletSelectorOptions } = Components;
 const SIGNER_POINTER = "reef-app-signer-pointer";
@@ -231,12 +43,12 @@ export const connectWalletConnect = async (
     value: true,
     message: "initializing mobile app connection",
   });
-  setSelExtensionName(undefined); //force setting this to different value from the ident initially or else it doesn't call useInitReefState hook
+  setSelExtensionName(undefined);
 
   const response: reefExt.WcConnection | undefined =
     await connectWc(setWcPreloader);
-  console.log({ response });
-  console.log("connectWalletConnect", response);
+  
+ 
   if (response) {
     reefExt.injectWcAsExtension(response, {
       name: reefExt.REEF_WALLET_CONNECT_IDENT,
@@ -296,7 +108,6 @@ function App() {
   };
   const selectedNetwork = useMemo(() => {
     if (!!network) {
-      console.log({ network });
       return network.name;
     }
     return undefined;
@@ -308,18 +119,13 @@ function App() {
   ];
 
   useEffect(() => {
-    console.log({ loading });
     if (!loading && isNetworkSwitching) setNetworkSwitching(false);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+   
   }, [loading]);
 
   useEffect(() => {
-    console.log({ error });
     if (error?.code === 1) {
       setSelExtensionName(undefined);
-    }
-    if (selExtensionName === reefExt.REEF_SNAP_IDENT && error?.code === 2) {
-      //history.push(SNAP_URL);
     }
   }, [extension, error]);
 
@@ -335,14 +141,12 @@ function App() {
     setUserBalance(convertToReadableFormat(selectedReefSigner?.balance._hex));
     //getTokenSymbol(selectedReefSigner);
     reefState.setAccounts(signers);
-    //getDAOToken(reefState.signer);
-    console.log({ signers, balance: selectedReefSigner?.balance, network });
+
     if (signers?.length && signers?.indexOf(selectedReefSigner!) == -1) {
       reefState.setSelectedAddress(signers[0].address);
       reefState.setAccounts(signers);
       setUserBalance(convertToReadableFormat(selectedReefSigner?.balance._hex));
-      getTokenSymbol(selectedReefSigner);
-      console.log({ selectedReefSigner });
+      
     }
   }, [selectedReefSigner, signers]);
   const appAvailableNetworks = [
@@ -350,36 +154,19 @@ function App() {
     nw.AVAILABLE_NETWORKS.testnet,
   ];
   const onExtensionSelected = async (ident: string) => {
-    console.log("extension changed to ", ident);
     if (ident === reefExt.REEF_WALLET_CONNECT_IDENT) {
       await connectWalletConnect(ident, setSelExtensionName, setWcPreloader);
     } else {
       setSelExtensionName(ident);
     }
   };
-  const convertToReadableFormat = (value) => {
-    console.log({ value });
-    const decimalValue = BigNumber.from(!!value ? value : 0);
-    return decimalValue.toBigInt();
-  };
-  const handleDropdownItemClick = (extension: string) => {
-    onExtensionSelected(extension);
-  };
+ 
 
-  const connectWallet = (extension: string) => {
-    onExtensionSelected(extension);
-  };
-
-  const handleConnectClick = () => {
-    console.log("connect clicked");
-    setDropdownOpen(!dropdownOpen);
-  };
 
   const selectNetwork = (key: "mainnet" | "testnet"): void => {
     setSwitchingNetwork(false);
     const toSelect = appAvailableNetworks.find((item) => item.name === key);
     networkSwitch.setSwitching(true);
-    console.log({ toSelect });
 
     if (toSelect) {
       reefState.setSelectedNetwork(toSelect);
@@ -389,46 +176,11 @@ function App() {
 
   const selectAccount = (index: number | null): void => {
     saveSignerLocalPointer(index || 0);
-    console.log({ index });
     reefState.setSelectedAddress(
       index != null ? accounts?.[index].address : undefined,
     );
   };
 
-  const handleSelectWallet = (index) => {
-    reefState.setSelectedAddress(signers[index].address);
-    setUserBalance(convertToReadableFormat(selectedReefSigner?.balance._hex));
-  };
-
-  /*const handleNetworkChange = (key: string) => {
-    //console.log('network changed to ', key);
-    setSelectedNetwork(key);
-    //const networkToChange = appAvailableNetworks.find((network) => network.name === key);
-    //reefState.setSelectedNetwork(networkToChange!);
-  }*/
-
-  const getUserBalance = async (account) => {
-    const contract = getDAOToken(account, network.name);
-    const userBalance =
-      !!contract && (await contract.balanceOf(account.evmAddress));
-    console.log({ userBalance });
-    setUserBalanceERC20(convertToReadableFormat(userBalance._hex));
-  };
-
-  const getTokenSymbol = async (account) => {
-    const contract = getDAOToken(account, network.name);
-    const tokenSymbol = !!contract && (await contract.symbol());
-    console.log({ tokenSymbol });
-    setTokenSymbol(tokenSymbol);
-    //return tokenSymbol;
-  };
-
-  const transfert = async (account) => {
-    const contract = getDAOToken(account, network.name);
-    const tx = !!contract && (await contract.transfer(walletDestination, 1000));
-    setTransactionHash(tx);
-    console.log({ tx });
-  };
 
   return (
     <>
